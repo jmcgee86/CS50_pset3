2e09be8c7f6d48e57ca9b53d8b7f214dc451f641
Jim McGee <jmcgee86@gmail.com>
2018-04-07 19:38:11 +0000

    add solution for find-less

--- find/helpers.c
+++ find/helpers.c
@@ -18,16 +18,15 @@ if(n < 0)
     return false;
 }
 int left = 0;
-int right = n;
+int right = n-1;
 
 while (n > 0)
 {
-    //int left = 0;
-    int middle = (right/2) + (left/2);
-    //int right = n-1
 
-   // if (value == values[((n - 1) / 2)])
-   if (value ==values[middle])
+    //int middle = (right/2) + (left/2);
+    int middle = (right+left)/2;
+
+   if (value == values[middle])
     {
         return true;
     }
@@ -36,6 +35,8 @@ while (n > 0)
        // values= right side of array
         n = n/2; //length of right side of array
         left = middle + 1;
+        //right = n;
+
         //right = n - 1;
     }
     else if (value< values[middle])
@@ -44,6 +45,9 @@ while (n > 0)
         n = n/2; //length of left side array
 
         right = middle - 1;
+        //if (right<0){
+          //  right = 0;
+        //}
     }
 }
 
@@ -55,6 +59,29 @@ return false;
  */
 void sort(int values[], int n)
 {
-    // TODO: implement a sorting algorithm
-    return;
+int lowest;
+int index;
+for(int i = 0; i<n; i++)
+{
+    for (int j = i+1; j<n; j++)
+    {
+        index = i;
+        lowest = values[i];
+        //lowest = values[j];
+        if (values[j]<lowest)
+        {
+            lowest = values[j];
+            index = j;
+        }
+        if (lowest<values[i])
+        {
+            values[index] = values[i];
+            values[i] = lowest;
+           // values[index] = values[i];
+        }
+    }
+    //index = 0;
+    //lowest
+}
+return;
 }
