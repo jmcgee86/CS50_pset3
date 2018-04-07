d57adeb76f7ef4e8fb127c89fd8baa1560de8979
Jim McGee <jmcgee86@gmail.com>
2018-04-07 16:56:34 +0000

    search function working, need to implement sort

--- find/helpers.c
+++ find/helpers.c
@@ -3,7 +3,7 @@
  *
  * Helper functions for Problem Set 3.
  */
- 
+
 #include <cs50.h>
 
 #include "helpers.h"
@@ -13,8 +13,41 @@
  */
 bool search(int value, int values[], int n)
 {
-    // TODO: implement a searching algorithm
+if(n < 0)
+{
     return false;
+}
+int left = 0;
+int right = n;
+
+while (n > 0)
+{
+    //int left = 0;
+    int middle = (right/2) + (left/2);
+    //int right = n-1
+
+   // if (value == values[((n - 1) / 2)])
+   if (value ==values[middle])
+    {
+        return true;
+    }
+    else if (value > values[middle])
+    {
+       // values= right side of array
+        n = n/2; //length of right side of array
+        left = middle + 1;
+        //right = n - 1;
+    }
+    else if (value< values[middle])
+    {
+       // values = left side of array
+        n = n/2; //length of left side array
+
+        right = middle - 1;
+    }
+}
+
+return false;
 }
 
 /**
