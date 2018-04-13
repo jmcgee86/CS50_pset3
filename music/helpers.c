6aa76342dc9b079b96c87ececc03a1c1188fa6de
Jim McGee <jmcgee86@gmail.com>
2018-04-13 15:07:26 +0000

    fix style for music

--- music/helpers.c
+++ music/helpers.c
@@ -22,7 +22,7 @@ int duration(string fraction)
     d[0] = fraction[2];
     numerator = atoi(n);
     denominator = atoi(d);
-    mth = (numerator / denominator) *8;
+    mth = (numerator / denominator) * 8;
     dur = round(mth);
     return dur;
 }
@@ -37,12 +37,12 @@ int frequency(string note)
     float octave;
     float hz;
     int hertz;
-    if(!isdigit(note[1]))
+    if (!isdigit(note[1]))
     {
         fs_note[0] = note[0];
         fs_note[1] = note[1];
         fs_note[2] = '\0';
-        in_note=fs_note;
+        in_note = fs_note;
         oct[0] = note[2];
         oct[1] = '\0';
     }
@@ -56,65 +56,65 @@ int frequency(string note)
     }
     octave = atoi(oct);
     if (!strcmp(in_note, "A"))
-        {
-            hz = 440 * pow(2, octave-4);
-            hertz = round(hz);
-        }
-    else if(!strcmp(in_note, "A#") || (!strcmp(in_note, "Bb")))
     {
-        hz = 440 * pow(2, octave-4) * pow(2, 1.0/12);
+        hz = 440 * pow(2, octave - 4);
         hertz = round(hz);
     }
-    else if(!strcmp(in_note, "B"))
+    else if (!strcmp(in_note, "A#") || (!strcmp(in_note, "Bb")))
     {
-        hz = 440 * pow(2, octave-4) * pow(2, 2.0/12);
+        hz = 440 * pow(2, octave - 4) * pow(2, 1.0 / 12);
         hertz = round(hz);
     }
-    else if(!strcmp(in_note, "Ab")|| !strcmp(in_note, "G#"))
+    else if (!strcmp(in_note, "B"))
     {
-        hz = 440 * pow(2, octave-4) * pow(2, -1.0/12);
+        hz = 440 * pow(2, octave - 4) * pow(2, 2.0 / 12);
         hertz = round(hz);
     }
-    else if(!strcmp(in_note, "G"))
+    else if (!strcmp(in_note, "Ab") || !strcmp(in_note, "G#"))
     {
-        hz = 440 * pow(2, octave-4) * pow(2, -2.0/12);
+        hz = 440 * pow(2, octave - 4) * pow(2, -1.0 / 12);
         hertz = round(hz);
     }
-    else if(!strcmp(in_note, "F#") || !strcmp(in_note, "Gb"))
+    else if (!strcmp(in_note, "G"))
     {
-        hz = 440 * pow(2, octave-4) * pow(2, -3.0/12);
+        hz = 440 * pow(2, octave - 4) * pow(2, -2.0 / 12);
         hertz = round(hz);
     }
-    else if(!strcmp(in_note, "F"))
+    else if (!strcmp(in_note, "F#") || !strcmp(in_note, "Gb"))
     {
-        hz = 440 * pow(2, octave-4) * pow(2, -4.0/12);
+        hz = 440 * pow(2, octave - 4) * pow(2, -3.0 / 12);
         hertz = round(hz);
     }
-    else if(!strcmp(in_note, "E"))
+    else if (!strcmp(in_note, "F"))
     {
-        hz = 440 * pow(2, octave-4) * pow(2, -5.0/12);
+        hz = 440 * pow(2, octave - 4) * pow(2, -4.0 / 12);
         hertz = round(hz);
     }
-    else if(!strcmp(in_note, "D#") || !strcmp(in_note, "Eb"))
+    else if (!strcmp(in_note, "E"))
     {
-        hz = 440 * (pow(2, octave-4) * pow(2, (-6.0/12)));
+        hz = 440 * pow(2, octave - 4) * pow(2, -5.0 / 12);
         hertz = round(hz);
-
     }
-    else if(!strcmp(in_note, "D"))
+    else if (!strcmp(in_note, "D#") || !strcmp(in_note, "Eb"))
     {
-        hz = 440 * pow(2, octave-4) * pow(2, -7.0/12);
+        hz = 440 * (pow(2, octave - 4) * pow(2, (-6.0 / 12)));
         hertz = round(hz);
 
     }
-    else if(!strcmp(in_note, "C#") || !strcmp(in_note, "Db"))
+    else if (!strcmp(in_note, "D"))
     {
-        hz = 440 * pow(2, octave-4) * pow(2, -8.0/12);
+        hz = 440 * pow(2, octave - 4) * pow(2, -7.0 / 12);
+        hertz = round(hz);
+
+    }
+    else if (!strcmp(in_note, "C#") || !strcmp(in_note, "Db"))
+    {
+        hz = 440 * pow(2, octave - 4) * pow(2, -8.0 / 12);
         hertz = round(hz);
     }
     else
     {
-        hz = 440 * pow(2, octave-4) * pow(2, -9.0/12);
+        hz = 440 * pow(2, octave - 4) * pow(2, -9.0 / 12);
         hertz = round(hz);
     }
 
@@ -124,14 +124,14 @@ int frequency(string note)
 // Determines whether a string represents a rest
 bool is_rest(string s)
 {
-string check = "";
-if (!strcmp(s, check))
-{
-    return 1;
-}
-else
-{
-    return 0;
-}
+    string check = "";
+    if (!strcmp(s, check))
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
 
 }
