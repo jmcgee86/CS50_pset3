54d5b66f9e35e20c0b278857a4ffeb25492df4c5
Jim McGee <jmcgee86@gmail.com>
2018-04-15 12:57:17 +0000

    fix style and add comments for find

--- find/helpers.c
+++ find/helpers.c
@@ -13,75 +13,60 @@
  */
 bool search(int value, int values[], int n)
 {
-if(n < 0)
-{
-    return false;
-}
-int left = 0;
-int right = n-1;
-
-while (n > 0)
-{
-
-    //int middle = (right/2) + (left/2);
-    int middle = (right+left)/2;
-
-   if (value == values[middle])
+    if (n < 0) // if there are no values in array, returns false
     {
-        return true;
+        return false;
     }
-    else if (value > values[middle])
+
+    int left = 0; //keeps track of left side index of array, starts at index 0
+    int right = n - 1; //keeps track of right side index of array, starts at end (n-1)
+
+    while (n > 0) //while there are still numbers to check
     {
-       // values= right side of array
-        n = n/2; //length of right side of array
-        left = middle + 1;
-        //right = n;
+        int middle = (right + left) / 2; //finds the middle index of array
 
-        //right = n - 1;
+        if (value == values[middle]) //if the value we are trying to find equals value of int at middle index of array, return true
+        {
+            return true;
+        }
+        else if (value > values[middle]) //if value is greater, we need to search right side of array
+        {
+            n = n / 2; //updates length of array we are looking at (just the right side)
+            left = middle + 1; //updates the left side of the array, we already checked middle so we set to middle + 1
+        }
+        else if (value < values[middle]) // if value is less than, we need to search left side of array
+        {
+            n = n / 2; //updates length of array we are looking at (just left side)
+            right = middle - 1; //updates right side of array
+        }
     }
-    else if (value< values[middle])
-    {
-       // values = left side of array
-        n = n/2; //length of left side array
-
-        right = middle - 1;
-        //if (right<0){
-          //  right = 0;
-        //}
-    }
-}
-
-return false;
+    return false; //if value is not in values array, return false
 }
 
 /**
  * Sorts array of n values.
  */
-void sort(int values[], int n)
+void sort(int values[], int n) //sorts values array smallest to largest
 {
-int lowest;
-int index;
-for(int i = 0; i<n; i++)
-{
-    for (int j = i+1; j<n; j++)
+    int lowest; //keeps track of lowest number
+    int index; // keeps track of index we are working on
+    for (int i = 0; i < n; i++) // iterates through array starting at index 0
     {
-        index = i;
-        lowest = values[i];
-        //lowest = values[j];
-        if (values[j]<lowest)
+        for (int j = i + 1; j < n; j++) // finds the lowest number in rest of array to place in index we are working on
         {
-            lowest = values[j];
-            index = j;
-        }
-        if (lowest<values[i])
-        {
-            values[index] = values[i];
-            values[i] = lowest;
-           // values[index] = values[i];
+            index = i;
+            lowest = values[i];
+            if (values[j] < lowest) //finds lowest value in rest of array
+            {
+                lowest = values[j];
+                index = j;
+            }
+            if (lowest < values[i]) //if lowest value found is less than value in index, switches these two values
+            {
+                values[index] = values[i];
+                values[i] = lowest;
+            }
         }
     }
-    //index = 0;
-    //lowest
-}
-return;
+    return;
 }
